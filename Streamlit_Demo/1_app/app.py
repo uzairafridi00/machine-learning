c0459ec396d416755a6ce59e80e4f0eef4bd606c
Uzair Afridi <uzairafridi00@gmail.com>
2024-03-21 15:28:30 +0500

    Streamlit Word Count App uploaded

--- Streamlit_Demo/1_app/app.py
+++ Streamlit_Demo/1_app/app.py
@@ -51,4 +51,52 @@ else:
 
 # display the summary statistics of the selected one
 st.write('Summary Statistics:', df.describe())
-    
+
+
+# # selected the specific columns For X and Y axis from the dataset and also select the plot
+# x_axis = st.selectbox('Select X Axis', df.columns)
+# y_axis = st.selectbox('Select Y Axis', df.columns)
+# plot_type = st.selectbox('Select Plot Type',['line','scatter','bar','hist','box','kde'])
+
+
+# # plot the data
+# if plot_type == 'line':
+#     st.line_chart(df[[x_axis,y_axis]])
+# elif plot_type == 'scatter':
+#     st.scatter_chart(df[[x_axis,y_axis]])
+# elif plot_type == 'bar':
+#     st.bar_chart(df[[x_axis,y_axis]])
+# elif plot_type == 'hist':
+#     df[x_axis].plot(kind='hist')
+#     st.pyplot()
+# elif plot_type == 'box':
+#     df[[x_axis,y_axis]].plot(kind='box')
+#     st.pyplot()
+# elif plot_type == 'kde':
+#     df[[x_axis,y_axis]].plot(kind='kde')
+#     st.pyplot() 
+
+
+# Create a pairplot
+
+st.subheader('Pair Plot')
+
+# select the column to used as hue
+hue_column = st.selectbox('Select a column to be used as hue',df.columns)
+st.pyplot(sns.pairplot(df,hue=hue_column))
+
+
+# create a heatmap
+st.subheader('Heat Map')
+# select columns which are numeric and then create a corr_matrix
+numeric_columns = df.select_dtypes(include=np.number).columns
+corr_matrix = df[numeric_columns].corr()
+
+from plotly import graph_objects as go
+# Convert the seaborn heatmap plot to a Plotly figure
+heatmap_fig = go.Figure(data=go.Heatmap(z=corr_matrix.values,
+                                       x=corr_matrix.columns,
+                                       y=corr_matrix.columns,
+                                       colorscale='Viridis'))
+st.plotly_chart(heatmap_fig)
+
