3b35dae3c5c67857c968303f6bc16d9c341fd30b
Uzair Afridi <uzairafridi00@gmail.com>
2024-08-28 15:13:22 +0500

    Flask tutorial

---
+++ Flask/first_flask_app.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! this is the Main <h1>Index Page<h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}"
+
+@app.route("/dashboard")
+def dashboard():
+    return "<h1>Welcome to Dashboard<h1>"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("dashboard"))
+
+if __name__ == "__main__":
+    app.run()
